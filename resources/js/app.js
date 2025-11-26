3f41d114f26e06c03ab68ea517c3091bc419c6cb
djgraphics28 <darwin.ibay30@gmail.com>
2025-11-26 11:28:18 +0800

    added chat system

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,11 @@ if ('serviceWorker' in navigator) {
         console.log(`SW registration failed`);
     });
 }
+
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allow your team to quickly build robust real-time web applications.
+ */
+
+import './echo';
